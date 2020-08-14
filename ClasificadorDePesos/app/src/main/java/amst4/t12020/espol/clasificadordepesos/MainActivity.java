d860afd6aa9c1ac2383fed945e9c42cb4e47aa21
Jodezamb <67130967+Jodezamb@users.noreply.github.com>
2020-08-13 20:09:44 -0500

    Actualizacion

--- ClasificadorDePesos/app/src/main/java/amst4/t12020/espol/clasificadordepesos/MainActivity.java
+++ ClasificadorDePesos/app/src/main/java/amst4/t12020/espol/clasificadordepesos/MainActivity.java
@@ -39,7 +39,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
