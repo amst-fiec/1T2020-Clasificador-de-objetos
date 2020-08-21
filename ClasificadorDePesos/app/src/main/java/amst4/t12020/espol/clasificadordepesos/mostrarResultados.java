7569377ee1d626359a4d1fbb6fc387f46484b1d6
Jodezamb <67130967+Jodezamb@users.noreply.github.com>
2020-08-21 03:59:28 -0500

    Mostrar resultados por pantalla desde la base de datos en firebase

---
+++ ClasificadorDePesos/app/src/main/java/amst4/t12020/espol/clasificadordepesos/mostrarResultados.java
@@ -0,0 +1,14 @@
+package amst4.t12020.espol.clasificadordepesos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mostrarResultados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mostrar_resultados);
+    }
+}
